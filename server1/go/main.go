33bbf56b674ef5b6780958f820db1d2aa9d9390e
Takec-a <atcoder1981se@gmail.com>
2023-11-25 16:44:51 +0900

    app1台　db2台構成に

--- server1/go/main.go
+++ server1/go/main.go
@@ -30,6 +30,7 @@ const (
 var (
 	powerDNSSubdomainAddress string
 	dbConn                   *sqlx.DB
+	dbConn2                  *sqlx.DB
 	secret                   = []byte("isucon13_session_cookiestore_defaultsecret")
 )
 
@@ -48,6 +49,68 @@ func connectDB(logger echo.Logger) (*sqlx.DB, error) {
 	const (
 		networkTypeEnvKey = "ISUCON13_MYSQL_DIALCONFIG_NET"
 		addrEnvKey        = "ISUCON13_MYSQL_DIALCONFIG_ADDRESS"
+		portEnvKey        = "ISUCON13_MYSQL_DIALCONFIG_PORT"
+		userEnvKey        = "ISUCON13_MYSQL_DIALCONFIG_USER"
+		passwordEnvKey    = "ISUCON13_MYSQL_DIALCONFIG_PASSWORD"
+		dbNameEnvKey      = "ISUCON13_MYSQL_DIALCONFIG_DATABASE"
+		parseTimeEnvKey   = "ISUCON13_MYSQL_DIALCONFIG_PARSETIME"
+	)
+
+	conf := mysql.NewConfig()
+
+	// 環境変数がセットされていなかった場合でも一旦動かせるように、デフォルト値を入れておく
+	// この挙動を変更して、エラーを出すようにしてもいいかもしれない
+	conf.Net = "tcp"
+	conf.Addr = net.JoinHostPort("127.0.0.1", "3306")
+	conf.User = "isucon"
+	conf.Passwd = "isucon"
+	conf.DBName = "isupipe"
+	conf.ParseTime = true
+	conf.InterpolateParams = true // ADMIN PREPARE対策
+	if v, ok := os.LookupEnv(networkTypeEnvKey); ok {
+		conf.Net = v
+	}
+	if addr, ok := os.LookupEnv(addrEnvKey); ok {
+		if port, ok2 := os.LookupEnv(portEnvKey); ok2 {
+			conf.Addr = net.JoinHostPort(addr, port)
+		} else {
+			conf.Addr = net.JoinHostPort(addr, "3306")
+		}
+	}
+	if v, ok := os.LookupEnv(userEnvKey); ok {
+		conf.User = v
+	}
+	if v, ok := os.LookupEnv(passwordEnvKey); ok {
+		conf.Passwd = v
+	}
+	if v, ok := os.LookupEnv(dbNameEnvKey); ok {
+		conf.DBName = v
+	}
+	if v, ok := os.LookupEnv(parseTimeEnvKey); ok {
+		parseTime, err := strconv.ParseBool(v)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse environment variable '%s' as bool: %+v", parseTimeEnvKey, err)
+		}
+		conf.ParseTime = parseTime
+	}
+
+	db, err := sqlx.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(10)
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectDB2(logger echo.Logger) (*sqlx.DB, error) {
+	const (
+		networkTypeEnvKey = "ISUCON13_MYSQL_DIALCONFIG_NET"
+		addrEnvKey        = "ISUCON13_MYSQL_DIALCONFIG_ADDRESS2"
 		portEnvKey        = "ISUCON13_MYSQL_DIALCONFIG_PORT"
 		userEnvKey        = "ISUCON13_MYSQL_DIALCONFIG_USER"
 		passwordEnvKey    = "ISUCON13_MYSQL_DIALCONFIG_PASSWORD"
@@ -192,6 +255,15 @@ func main() {
 	}
 	defer conn.Close()
 	dbConn = conn
+
+	// DB接続2
+	conn2, err2 := connectDB2(e.Logger)
+	if err2 != nil {
+		e.Logger.Errorf("failed to connect db: %v", err2)
+		os.Exit(1)
+	}
+	defer conn2.Close()
+	dbConn2 = conn2
 
 	subdomainAddr, ok := os.LookupEnv(powerDNSSubdomainAddressEnvKey)
 	if !ok {
